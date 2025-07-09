cf5724209ed0848c4ea9f8b7f946bc043fd73cf9
Luuk van der Duim <luukvanderduim@gmail.com>
2025-07-09 16:14:34 +0200

    Add assertions to verify `Peers`-list mutations
    - Adds assertions
    - Rewritten README.md

--- README.md
+++ README.md
@@ -1,43 +1,54 @@
 # test-atspi-p2p
 
-This tests the `p2p` branch of [`atspi`](<http://github.com/odilia-app/atspi.git>)
+The P2P feature of [`atspi`](<http://github.com/odilia-app/atspi.git>) spawns a task that listens for [`NameOwnerChanged`](<https://dbus.freedesktop.org/doc/dbus-java/api/index.html?org/freedesktop/DBus.NameOwnerChanged.html>) events.
+
+This `DBus` signal is used to update the `Peers` list - applications that support P2P - in `AccessibilityConnection`.
+
+We start a known supported application, verify whether it gets added, then terminate the same application and verify whether it gets removed from the list.
+
+What is currently not verified:
+
+- Entry of a `WellKnownName`
+- Transfer of ownership of a `WellKnownName`
+
+To verify this behavior, we need an example of such an application.
+Suggestions are welcome.
+
+See: [`atspi`](http://github.com/odilia-app/atspi.git)
 
 Currently it:
 
-- subscribes to tracing messages
-- shows the last three applications in the p2p `Peers` list of the connection
-- starts an application (gedit)
-- prints the last three applications
-- terminates the application a few seconds later.
-- prints the last three applications
+1. Subscribes to tracing messages
+2. Shows the last three applications in the `Peers` list
+3. Launches an application and verifies it appears in the peer list
+4. Terminates the application and verifies it gets removed from the peer list
 
-This way one can see that chreation of initial peers works as well as insertion and deletion when new peers appear on the bus.
+This demonstrates that peer insertion and deletion work correctly when applications join and leave the accessibility bus.
 
 ## Example output
 
 ```bash
 ~/c/test-atspi-p2p (main)> cargo r
-    Finished `dev` profile [unoptimized] target(s) in 0.06s
+    Finished `dev` profile [unoptimized] target(s) in 0.37s
      Running `target/debug/test-atspi-p2p`
 Starting accessibility connection with P2P support
-2025-07-05T15:03:46.245981Z  INFO main ThreadId(01) new: Connecting to a11y bus
-2025-07-05T15:03:46.247151Z  INFO main ThreadId(01) new: Connected to a11y bus name=":1.81"
+2025-07-09T13:49:11.408336Z  INFO main ThreadId(01) new: Connecting to a11y bus
+2025-07-09T13:49:11.408862Z  INFO main ThreadId(01) new: Connected to a11y bus name=":1.59"
 Last three `Peers`:
-Peer: ":1.30", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-XYU482/socket"
-Peer: ":1.36", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-DGC582/socket"
-Peer: ":1.69", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-V96X82/socket"
-launching "gedit"
-2025-07-05T15:03:46.551893Z  INFO zbus::Connection executor ThreadId(28) Inserted unique name: :1.82 into the peer list.
+Peer: ":1.23", well-known name: "None". human readable name: "blueman-applet"
+Peer: ":1.25", well-known name: "None". human readable name: "blueman-tray"
+Peer: ":1.31", well-known name: "None". human readable name: "code-insiders"
+Launching "firefox"
+2025-07-09T13:49:12.173975Z  INFO tokio-runtime-worker ThreadId(20) Inserted unique name: :1.60 into the peer list.
 Last three `Peers`:
-Peer: ":1.36", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-DGC582/socket"
-Peer: ":1.69", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-V96X82/socket"
-Peer: ":1.82", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-NRUW82/socket"
-terminating "gedit"
-2025-07-05T15:03:52.320056Z  INFO zbus::Connection executor ThreadId(28) Peer with unique name: :1.82 left the bus.
+Peer: ":1.25", well-known name: "None". human readable name: "blueman-tray"
+Peer: ":1.31", well-known name: "None". human readable name: "code-insiders"
+Peer: ":1.60", well-known name: "None". human readable name: "Firefox"
+Terminating "firefox"
+2025-07-09T13:49:15.468729Z  INFO tokio-runtime-worker ThreadId(20) Peer with unique name: :1.60 left the bus - removed from peer list.
 Last three `Peers`:
-Peer: ":1.30", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-XYU482/socket"
-Peer: ":1.36", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-DGC582/socket"
-Peer: ":1.69", well-known name: "None". address: "unix:path=/run/user/1000/at-spi2-V96X82/socket"
+Peer: ":1.23", well-known name: "None". human readable name: "blueman-applet"
+Peer: ":1.25", well-known name: "None". human readable name: "blueman-tray"
+Peer: ":1.31", well-known name: "None". human readable name: "code-insiders"
 done
-
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::sync::Arc;
 
 use async_lock::Mutex;
+use atspi::ObjectRef;
 use atspi::connection;
 use atspi::connection::AccessibilityConnection;
 use atspi::connection::P2P;
 use atspi::connection::Peer;
-
+use atspi::proxy::accessible::ObjectRefExt;
+use pretty_assertions::assert_eq;
 use tracing_subscriber::fmt;
+use zbus::names::OwnedBusName;
+use zbus::names::OwnedUniqueName;
+use zbus::zvariant::ObjectPath;
 
-static APP_NAME: &str = "gedit";
+static APP_NAME: &str = "firefox";
+static APP_ARG: &str = "";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    atspi::connection::set_session_accessibility(true)
+        .await
+        .expect("Failed to set session accessibility");
+
     // Configure a custom event formatter
     let format = fmt::format()
         .with_level(true) // don't include levels in formatted output
@@ -33,32 +43,75 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let peers = a11y.peers().await;
 
-    print_peers(peers.clone()).await;
+    let mapping = bus_names_to_human_readable(&a11y).await;
+    print_peers(peers.clone(), &mapping).await;
 
-    println!("launching \"{APP_NAME}\"");
+    let last_busname_before = peers
+        .lock()
+        .await
+        .last()
+        .map(|p| p.unique_name().to_string())
+        .unwrap_or_else(|| "None".to_string());
+
+    println!("Launching \"{APP_NAME}\"");
     let mut child_process = std::process::Command::new(APP_NAME)
+        .arg(APP_ARG)
         .stdout(std::process::Stdio::null())
         .stderr(std::process::Stdio::null())
         .spawn()
         .expect("Failed to launch terminal");
 
-    std::thread::sleep(std::time::Duration::from_millis(3000));
-    print_peers(peers.clone()).await;
+    // Registry needs a bit of time to populate with the new app
+    std::thread::sleep(std::time::Duration::from_secs(2));
 
-    std::thread::sleep(std::time::Duration::from_millis(3000));
-    println!("terminating \"{APP_NAME}\"");
+    let launched_busname = peers
+        .lock()
+        .await
+        .last()
+        .map(|p| p.unique_name())
+        .unwrap()
+        .clone();
+    let launched_human_readable = to_human_readable(&launched_busname, &a11y).await;
+    assert_eq!(
+        launched_human_readable.to_lowercase(),
+        APP_NAME,
+        "The launched app's name should match the app name, but got: \"{launched_human_readable}\""
+    );
+
+    let mapping = bus_names_to_human_readable(&a11y).await;
+    print_peers(peers.clone(), &mapping).await;
+
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    println!("Terminating \"{APP_NAME}\"");
 
     child_process.kill().expect("Failed to kill process");
     child_process.wait().expect("Failed to wait on process");
+    std::thread::sleep(std::time::Duration::from_secs(1));
 
-    std::thread::sleep(std::time::Duration::from_millis(3000));
-    print_peers(peers.clone()).await;
+    let mapping = bus_names_to_human_readable(&a11y).await;
+    print_peers(peers.clone(), &mapping).await;
+
+    let last_busname_after = peers
+        .lock()
+        .await
+        .last()
+        .map(|p| p.unique_name().to_string())
+        .unwrap_or_else(|| "None".to_string());
+
+    assert_eq!(
+        last_busname_before, last_busname_after,
+        "The last peer before launch and after termination should be the same, \
+         but they differ: before: \"{last_busname_before}\", after: \"{last_busname_after}\""
+    );
 
     println!("done");
     Ok(())
 }
 
-async fn print_peers(peers: Arc<Mutex<Vec<Peer>>>) {
+async fn print_peers(
+    peers: Arc<Mutex<Vec<Peer>>>,
+    bus_name_to_natural_name: &[(OwnedBusName, String)],
+) {
     println!("Last three `Peers`:");
     let peers = peers.lock().await;
 
@@ -70,11 +123,67 @@ async fn print_peers(peers: Arc<Mutex<Vec<Peer>>>) {
             well_known_name = name.to_string();
         }
 
+        let human_readable = bus_name_to_natural_name
+            .iter()
+            .find(|(bus_name, _)| **bus_name == **peer.unique_name())
+            .map(|(_, name)| name.clone());
+
         println!(
-            "Peer: \"{}\", well-known name: {:?}. address: \"{}\"",
+            "Peer: \"{}\", well-known name: {:?}. human readable name: \"{}\"",
             peer.unique_name(),
             well_known_name,
-            peer.socket_address()
+            human_readable.unwrap_or_else(|| "Unknown".to_string())
         );
     }
+}
+
+// Gets the accessible apps in registry, returns a mapping of bus names to human readable names
+async fn bus_names_to_human_readable(
+    a11y: &AccessibilityConnection,
+) -> Vec<(OwnedBusName, String)> {
+    let conn = a11y.connection();
+
+    let registry_accessible = a11y
+        .root_accessible_on_registry()
+        .await
+        .expect("Failed to get root accessible on registry");
+
+    let children = registry_accessible
+        .get_children()
+        .await
+        .expect("Failed to get children of root accessible");
+
+    // Create a mapping of bus_names to human readable names
+    let mut bus_name_to_human_readable: Vec<(OwnedBusName, String)> = Vec::new();
+
+    for child in children {
+        let ap = child
+            .as_accessible_proxy(conn)
+            .await
+            .expect("Failed to get accessible proxy");
+
+        let natural_name = ap.name().await.expect("Failed to get name");
+        let bus_name: OwnedBusName = ap.inner().destination().to_owned().into();
+        bus_name_to_human_readable.push((bus_name, natural_name));
+    }
+
+    bus_name_to_human_readable
+}
+
+// Transforms a single bus name to a human readable name
+async fn to_human_readable(bus_name: &OwnedUniqueName, a11y: &AccessibilityConnection) -> String {
+    static ROOT_PATH: ObjectPath<'static> =
+        ObjectPath::from_static_str_unchecked("/org/a11y/atspi/accessible/root");
+    let conn = a11y.connection();
+
+    let root_object = ObjectRef::new(bus_name.as_ref(), ROOT_PATH.clone());
+    let root_accessible = root_object
+        .as_accessible_proxy(conn)
+        .await
+        .expect("Failed to get root accessible");
+
+    root_accessible
+        .name()
+        .await
+        .expect("Failed to get name of root accessible")
 }
